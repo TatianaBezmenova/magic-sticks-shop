ac486b25d848c2c296cb9f789c38e05e3443153a
Tatiana Bezmenova <t.bezmenova@mail.ru>
2021-07-08 23:57:34 +0700

    Добавлен поиск товаров

--- src/product/views.py
+++ src/product/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
 from .models import Product, ProductType
 
+import logging
+
+log = logging.getLogger(__name__)
 
 class ProductDetailView(DetailView):
     model = Product
@@ -24,6 +27,23 @@ class ProductDetailView(DetailView):
 class ProductListView(ListView):
     model = Product
     paginate_by = 20
+
+    def get_queryset(self):
+        queryset = super().get_queryset().prefetch_related('type')
+
+        if 'type' in self.request.GET:
+            queryset = queryset.filter(type_id=self.request.GET['type'])
+        if 'price_start' in self.request.GET:
+            queryset = queryset.filter(price__gte=self.request.GET['price_start'])
+        if 'price_end' in self.request.GET:
+            queryset = queryset.filter(price__lte=self.request.GET['price_end'])
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['product_type_list'] = ProductType.objects.all()
+        return context
 
 
 def product_type_detail(request, slug):
